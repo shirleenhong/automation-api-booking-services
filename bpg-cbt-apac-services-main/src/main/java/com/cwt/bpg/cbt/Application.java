af78abea8f1b3ac392a56f47c6e3a01701550383
puigterence <puigterence@carlsonwagonlit.com>
2018-04-03 15:06:52 +0800

    initial project structure

--- bpg-cbt-apac-services-main/src/main/java/com/cwt/bpg/cbt/Application.java
+++ bpg-cbt-apac-services-main/src/main/java/com/cwt/bpg/cbt/Application.java
@@ -1,7 +1,9 @@
 package com.cwt.bpg.cbt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args)
