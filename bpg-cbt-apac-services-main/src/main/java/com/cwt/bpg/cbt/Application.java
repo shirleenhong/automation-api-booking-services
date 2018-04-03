fd8d7fe6ed0957a0873881eca965a314714ed600
puigterence <puigterence@carlsonwagonlit.com>
2018-04-03 15:03:41 +0800

    initial project structure

---
+++ bpg-cbt-apac-services-main/src/main/java/com/cwt/bpg/cbt/Application.java
@@ -0,0 +1,11 @@
+package com.cwt.bpg.cbt;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+	
+	public static void main(String[] args)
+    {
+		SpringApplication.run(Application.class, args);
+    }
+}
