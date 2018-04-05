98742b9ba8e0a9bc724bc3f3d9f511dc08be5bcb
Rodolfo L. dela Pena Jr <rodolfo.delapena@carlsonwagonlit.com>
2018-04-05 17:56:11 +0800

    added token validation

--- bpg-cbt-apac-services-rest/src/main/java/com/cwt/bpg/cbt/services/rest/security/CustomFilter.java
+++ bpg-cbt-apac-services-rest/src/main/java/com/cwt/bpg/cbt/services/rest/security/CustomFilter.java
@@ -12,28 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
+import com.cwt.bpg.cbt.security.api.TokenApi;
+
 public class CustomFilter extends GenericFilterBean {
+
+	private TokenApi tokenApi;
+
+	public CustomFilter(TokenApi tokenApi) {
+		this.tokenApi = tokenApi;
+	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
 
 		String authHeader = httpRequest.getHeader("Authorization");
-        if (StringUtils.isBlank(authHeader) || !authHeader.startsWith("Bearer ")) {
-        	httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Authorization header needed");
-            return ;
-        }
+		if (StringUtils.isBlank(authHeader) || !authHeader.startsWith("Bearer ")) {
+			httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Authorization header needed");
+			return;
+		}
 
-        String token = authHeader.substring(7).trim();
-        try {
-        	System.out.println("token: "+ token);
-        } catch (Exception e) {
-        	httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token.");
-            return ;
-        }
+		String token = authHeader.substring(7).trim();
+		System.out.println("token: " + token);
+		if (!tokenApi.isTokenExist(token)) {
+			httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token.");
+			return;
+		}
 		chain.doFilter(request, response);
 	}
 
--- bpg-cbt-apac-services-rest/src/main/java/com/cwt/bpg/cbt/services/rest/security/WebSecurityConfig.java
+++ bpg-cbt-apac-services-rest/src/main/java/com/cwt/bpg/cbt/services/rest/security/WebSecurityConfig.java
@@ -1,17 +1,23 @@
 package com.cwt.bpg.cbt.services.rest.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.cwt.bpg.cbt.security.api.TokenApi;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private TokenApi tokenApi;
+	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new CustomFilter(), BasicAuthenticationFilter.class);
+        http.csrf().disable().addFilterBefore(new CustomFilter(tokenApi), BasicAuthenticationFilter.class);
     }
 }
