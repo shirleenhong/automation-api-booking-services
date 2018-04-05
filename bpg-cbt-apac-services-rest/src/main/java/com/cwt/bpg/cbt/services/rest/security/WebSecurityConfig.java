f71206dd91ec68bcbd0e8ff1e6fb496e1cc14d8d
Rodolfo L. dela Pena Jr <rodolfo.delapena@carlsonwagonlit.com>
2018-04-05 15:14:45 +0800

    added custom filter

---
+++ bpg-cbt-apac-services-rest/src/main/java/com/cwt/bpg/cbt/services/rest/security/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.cwt.bpg.cbt.services.rest.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new CustomFilter(), BasicAuthenticationFilter.class);
+    }
+}
