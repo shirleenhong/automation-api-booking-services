6f50a90c5e14f7e5a2ecdfc1fdd134a566fae797
puigterence <puigterence@carlsonwagonlit.com>
2018-04-06 17:25:16 +0800

    [Service] Calculate APAC Fees from Data Source

--- bpg-cbt-apac-services-exchange-order-module/bpg-cbt-apac-services-exchange-order-rest/src/main/java/com/cwt/bpg/cbt/exchange/order/ExchangeOrderController.java
+++ bpg-cbt-apac-services-exchange-order-module/bpg-cbt-apac-services-exchange-order-rest/src/main/java/com/cwt/bpg/cbt/exchange/order/ExchangeOrderController.java
@@ -1,5 +1,18 @@
 package com.cwt.bpg.cbt.exchange.order;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ExchangeOrderController {
 
+	@GetMapping(path="/products")
+	public @ResponseBody ResponseEntity<String> getProducts(@RequestParam String countryCode) {
+		return new ResponseEntity<>("Products", HttpStatus.OK);
+	}
 }
