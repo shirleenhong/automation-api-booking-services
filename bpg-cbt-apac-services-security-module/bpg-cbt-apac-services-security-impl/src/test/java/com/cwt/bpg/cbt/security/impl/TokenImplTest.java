f7561a973072a530e1103821a2527cdaa0b41f16
mosesbriancalma <mosesbrian.calma@carlsonwagonlit.com>
2018-04-10 16:19:37 +0800

    updated service fee impl

---
+++ bpg-cbt-apac-services-security-module/bpg-cbt-apac-services-security-impl/src/test/java/com/cwt/bpg/cbt/security/impl/TokenImplTest.java
@@ -0,0 +1,51 @@
+package com.cwt.bpg.cbt.security.impl;
+
+import java.io.IOException;
+
+import org.bson.Document;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cwt.bpg.cbt.mongodb.config.MongoDbConnection;
+import com.cwt.bpg.cbt.mongodb.config.mapper.DBObjectMapper;
+import com.cwt.bpg.cbt.security.api.TokenApi;
+import com.cwt.bpg.cbt.security.api.model.Token;
+import com.mongodb.client.FindIterable;
+
+@Service
+public class TokenImplTest {
+
+	private MongoDbConnection mongoDbConnection;
+
+	private DBObjectMapper dBObjectMapper;
+	
+	@Test
+	public void test() {
+		DBObjectMapper objectMapper = Mockito.mock(DBObjectMapper.class);
+		System.out.println(1);
+	}
+
+	public boolean isTokenExist(String token) {
+		
+		boolean isExist = false;
+		
+		FindIterable iterable = mongoDbConnection.getCollection(Token.COLLECTION).find(new Document("_id",token));
+
+		Token tokenResult = null;
+		try {
+			tokenResult = dBObjectMapper.mapDocumentToBean((Document) iterable.first(), Token.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(tokenResult != null) {
+			isExist = true;
+		}
+
+		return isExist;
+	}
+
+}
