60269bf17f62e321ade706ca2a68d625e9dca30d
mosesbriancalma <mosesbrian.calma@carlsonwagonlit.com>
2018-04-11 11:18:03 +0800

    updated the unit test

--- bpg-cbt-apac-services-security-module/bpg-cbt-apac-services-security-impl/src/test/java/com/cwt/bpg/cbt/security/impl/TokenImplTest.java
+++ bpg-cbt-apac-services-security-module/bpg-cbt-apac-services-security-impl/src/test/java/com/cwt/bpg/cbt/security/impl/TokenImplTest.java
@@ -1,51 +1,76 @@
 package com.cwt.bpg.cbt.security.impl;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 
 import org.bson.Document;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.cwt.bpg.cbt.mongodb.config.MongoDbConnection;
 import com.cwt.bpg.cbt.mongodb.config.mapper.DBObjectMapper;
 import com.cwt.bpg.cbt.security.api.TokenApi;
 import com.cwt.bpg.cbt.security.api.model.Token;
+import com.cwt.bpg.cbt.security.api.model.User;
 import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
 
-@Service
+@RunWith(MockitoJUnitRunner.class)
 public class TokenImplTest {
 
+	
+	@Mock
 	private MongoDbConnection mongoDbConnection;
 
+	@Mock
 	private DBObjectMapper dBObjectMapper;
 	
+	@InjectMocks
+	private TokenApi tokenApi = new TokenImpl();
+	
 	@Test
-	public void test() {
-		DBObjectMapper objectMapper = Mockito.mock(DBObjectMapper.class);
-		System.out.println(1);
+	public void shouldExist() throws IOException {
+		
+		FindIterable iterable = Mockito.mock(FindIterable.class);
+		MongoCollection collection = Mockito.mock(MongoCollection.class);
+		Document doc = Mockito.mock(Document.class);
+		Mockito.when(mongoDbConnection.getCollection(Mockito.anyString())).thenReturn(collection);
+		Mockito.when(collection.find(Mockito.any(Document.class))).thenReturn(iterable);
+		Mockito.when(iterable.first()).thenReturn(doc);
+		
+		Token tokenResult = Mockito.mock(Token.class);
+		tokenResult.setKey("t0K3nK3y");
+		User user = new User();
+		user.setUsername("JR");
+		
+		Mockito.when(dBObjectMapper.mapDocumentToBean(doc,Token.class)).thenReturn(tokenResult);
+		
+		
+		assertTrue(tokenApi.isTokenExist("tokenkey"));
+	}
+	
+	@Test
+	public void shouldNotExist() throws IOException {
+		
+		FindIterable iterable = Mockito.mock(FindIterable.class);
+		MongoCollection collection = Mockito.mock(MongoCollection.class);
+		Document doc = Mockito.mock(Document.class);
+		Mockito.when(mongoDbConnection.getCollection(Mockito.anyString())).thenReturn(collection);
+		Mockito.when(collection.find(Mockito.any(Document.class))).thenReturn(iterable);
+		Mockito.when(iterable.first()).thenReturn(doc);
+		
+		
+		Mockito.when(dBObjectMapper.mapDocumentToBean(doc,Token.class)).thenReturn(null);
+		
+		
+		assertFalse(tokenApi.isTokenExist("tokenkey"));
 	}
 
-	public boolean isTokenExist(String token) {
-		
-		boolean isExist = false;
-		
-		FindIterable iterable = mongoDbConnection.getCollection(Token.COLLECTION).find(new Document("_id",token));
-
-		Token tokenResult = null;
-		try {
-			tokenResult = dBObjectMapper.mapDocumentToBean((Document) iterable.first(), Token.class);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		if(tokenResult != null) {
-			isExist = true;
-		}
-
-		return isExist;
-	}
-
+	
 }
