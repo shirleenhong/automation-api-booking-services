b0035e5981ef52946d8ad4972805e10185e69d28
puigterence <puigterence@carlsonwagonlit.com>
2018-04-19 19:11:53 +0800

    change query url to path param

--- bpg-cbt-apac-services-exchange-order-module/bpg-cbt-apac-services-exchange-order-rest/src/main/java/com/cwt/bpg/cbt/exchange/order/ProductsController.java
+++ bpg-cbt-apac-services-exchange-order-module/bpg-cbt-apac-services-exchange-order-rest/src/main/java/com/cwt/bpg/cbt/exchange/order/ProductsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,10 @@ public class ProductsController {
 	@Autowired
 	ProductsApi exchangeOrderApi;
 
-	@GetMapping(path="/products")
+	@GetMapping(path="/products/{countryCode}")
 	@ResponseBody
 	public ResponseEntity<List<Product>> getProducts(
-			@RequestParam(required = true) String countryCode) {		
+			@PathVariable String countryCode) {		
 		
 		return new ResponseEntity<>(exchangeOrderApi.getProducts(countryCode), HttpStatus.OK);
 	}
