29250b89d4a0636fda2c082f9bd8b30f193a8838
puigterence <puigterence@carlsonwagonlit.com>
2018-04-25 11:33:53 +0800

    TASK: REST global error mapper (Rest unrecoverable error)

--- apac-services-exception-handler/src/main/java/com/cwt/bpg/cbt/exception/handler/ApacServiceExceptionHandler.java
+++ apac-services-exception-handler/src/main/java/com/cwt/bpg/cbt/exception/handler/ApacServiceExceptionHandler.java
@@ -5,17 +5,21 @@ import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+@ResponseBody
 public class ApacServiceExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Override
@@ -51,5 +55,22 @@ public class ApacServiceExceptionHandler extends ResponseEntityExceptionHandler 
 				error);
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(),
 				apiError.getStatus());
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(
+			HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status,
+			WebRequest request) {
+		
+		Throwable mostSpecificCause = ex.getMostSpecificCause();
+		 
+		List<String> errors = new ArrayList<String>();
+		errors.add(mostSpecificCause.getMessage());
+		
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(),
+				errors);
+		
+		return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(),
+				request);
 	}
 }
