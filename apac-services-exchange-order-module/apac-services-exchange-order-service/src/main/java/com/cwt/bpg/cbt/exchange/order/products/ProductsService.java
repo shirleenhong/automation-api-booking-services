db800c7120ac662d3afe5b23b060d1c14932e4c7
puigterence <puigterence@carlsonwagonlit.com>
2018-04-25 16:56:09 +0800

    added simple cache

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/products/ProductsService.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/products/ProductsService.java
@@ -3,6 +3,7 @@ package com.cwt.bpg.cbt.exchange.order.products;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.cwt.bpg.cbt.exchange.order.ProductsRepository;
@@ -14,6 +15,7 @@ public class ProductsService {
 	@Autowired 
 	private ProductsRepository productRepo;
 	
+	@Cacheable("products")
 	public List<Product> getProducts(String countryCode) {
 		
 		return productRepo.getProducts(countryCode);
