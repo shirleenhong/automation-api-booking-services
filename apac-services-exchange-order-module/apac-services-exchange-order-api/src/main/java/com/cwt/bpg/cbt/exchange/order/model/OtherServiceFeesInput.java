8c6e7a5f03de7322cfc9cec122a13ed86276d893
bobbyperez-cwt <bperez2@carlsonwagonlit.com>
2018-05-03 16:09:51 +0800

    sonar fixes

--- apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/OtherServiceFeesInput.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/OtherServiceFeesInput.java
@@ -8,81 +8,98 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class OtherServiceFeesInput implements Serializable {
-	
+
 	private static final long serialVersionUID = -5237125856544162255L;
-	
+
 	@NotEmpty
-	protected String fopType; 
-	protected BigDecimal sellingPrice;
-	protected boolean isMerchantFeeAbsorb;
-	protected boolean isMerchantFeeWaive;	
+	private String fopType;
+	private BigDecimal sellingPrice;
+	private boolean isMerchantFeeAbsorb;
+	private boolean isMerchantFeeWaive;
 	private String countryCode;
 	@NotEmpty
-	protected String clientType;
+	private String clientType;
 	@NotEmpty
-	protected String profileName;
+	private String profileName;
 	@NotNull
-	protected Double gstPercent;
-	protected BigDecimal nettCost;
+	private Double gstPercent;
+	private BigDecimal nettCost;
 
 	public OtherServiceFeesInput() {
 		this.sellingPrice = BigDecimal.ZERO;
 		this.gstPercent = new Double(0);
 	}
-	
+
 	public String getFopType() {
 		return fopType;
 	}
+
 	public void setFopType(String fopType) {
 		this.fopType = fopType;
 	}
+
 	public BigDecimal getSellingPrice() {
 		return sellingPrice;
 	}
+
 	public void setSellingPrice(BigDecimal sellingPrice) {
 		this.sellingPrice = sellingPrice;
 	}
+
 	public boolean isMerchantFeeAbsorb() {
 		return isMerchantFeeAbsorb;
 	}
+
 	public void setMerchantFeeAbsorb(boolean isMerchantFeeAbsorb) {
 		this.isMerchantFeeAbsorb = isMerchantFeeAbsorb;
 	}
+
 	public boolean isMerchantFeeWaive() {
 		return isMerchantFeeWaive;
 	}
+
 	public void setMerchantFeeWaive(boolean isMerchantFeeWaive) {
 		this.isMerchantFeeWaive = isMerchantFeeWaive;
-	}	
+	}
+
 	public String getCountryCode() {
 		return countryCode;
 	}
+
 	public void setCountryCode(String countryCode) {
 		this.countryCode = countryCode;
 	}
+
 	public String getClientType() {
 		return clientType;
 	}
+
 	public void setClientType(String clientType) {
 		this.clientType = clientType;
 	}
+
 	public String getProfileName() {
 		return profileName;
 	}
+
 	public void setProfileName(String profileName) {
 		this.profileName = profileName;
 	}
+
 	public Double getGstPercent() {
 		return gstPercent;
 	}
+
 	public void setGstPercent(Double gstPercent) {
 		this.gstPercent = gstPercent;
 	}
+
 	public BigDecimal getNettCost() {
 		return nettCost;
 	}
+
 	public void setNettCost(BigDecimal nettCost) {
 		this.nettCost = nettCost;
 	}
-	
+
 }
