7f9f36574743a2df69f8740e17a7cbbf9527c024
puigterence <puigterence@carlsonwagonlit.com>
2018-05-04 16:14:33 +0800

    add unit test

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/ExchangeOrderServiceTest.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/ExchangeOrderServiceTest.java
@@ -1,12 +1,21 @@
 package com.cwt.bpg.cbt.exchange.order;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.anyString;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.cwt.bpg.cbt.exchange.order.model.CurrencyCodeRoundRule;
 import com.cwt.bpg.cbt.exchange.order.model.MerchantFee;
 
 public class ExchangeOrderServiceTest {
@@ -16,6 +25,9 @@ public class ExchangeOrderServiceTest {
 	
 	@InjectMocks
 	private ExchangeOrderService service;
+	
+	@Mock
+	private CurrencyRepository currencyRepo;
 	
 	@Before
 	public void setUp() {
@@ -28,7 +40,7 @@ public class ExchangeOrderServiceTest {
 		String clientType = "CL";
 		String profileName = "PN";
 		service.getMerchantFee(countryCode, clientType, profileName);
-		Mockito.verify(merchantFeeApi, Mockito.times(1)).getMerchantFee(countryCode, clientType, profileName);
+		verify(merchantFeeApi, times(1)).getMerchantFee(countryCode, clientType, profileName);
 	}
 	
 	
@@ -37,6 +49,18 @@ public class ExchangeOrderServiceTest {
 		MerchantFee fee = new MerchantFee();
 		service.putMerchantFee(fee);
 		Mockito.verify(merchantFeeApi, Mockito.times(1)).putMerchantFee(fee);
+	}
+	
+	@Test
+	public void canGetRoundingRule() {
+		CurrencyCodeRoundRule ccr = mock(CurrencyCodeRoundRule.class);
+		when(currencyRepo.getRoundingRule(anyString())).thenReturn(ccr);
+
+		CurrencyCodeRoundRule roundingRule = service.getRoundingRule("SG");
+		
+		verify(currencyRepo, times(1)).getRoundingRule(Mockito.anyString());
+		assertNotNull(roundingRule);
+		
 	}
 
 }
