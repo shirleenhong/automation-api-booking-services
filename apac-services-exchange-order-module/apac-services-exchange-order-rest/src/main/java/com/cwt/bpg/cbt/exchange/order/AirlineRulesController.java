e2ab1235406b0a508d10e41a6ff149b5de2dfbd6
bobbyperez-cwt <bperez2@carlsonwagonlit.com>
2018-05-18 10:29:19 +0800

    US622 [TECH] [India] Other Services Product and Vendor Codes added AirlineRuleService

--- apac-services-exchange-order-module/apac-services-exchange-order-rest/src/main/java/com/cwt/bpg/cbt/exchange/order/AirlineRulesController.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-rest/src/main/java/com/cwt/bpg/cbt/exchange/order/AirlineRulesController.java
@@ -1,16 +1,16 @@
 package com.cwt.bpg.cbt.exchange.order;
 
-import java.util.function.Supplier;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cwt.bpg.cbt.documentation.annotation.Internal;
+import com.cwt.bpg.cbt.exchange.order.model.AirlineRule;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -18,31 +18,30 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @Api(tags = "Airline Rules")
 public class AirlineRulesController {
+	@Autowired
+	private AirlineRuleService service;
 
 	@GetMapping(path = "/airline-rules")
 	@ResponseBody
 	@ApiOperation(value = "Pulls airline rules")
-	public ResponseEntity<?> getAirlineRules() {
-
-		return new ResponseEntity<>(null, HttpStatus.OK);
+	public ResponseEntity<List<AirlineRule>> getAirlineRules() {
+		return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
 	}
 
 	@PutMapping(path = "/airline-rules")
 	@ResponseBody
 	@ApiOperation(value = "Pulls airline rules")
 	@Internal
-	public ResponseEntity<?> putAirlineRules(Supplier<?> supplier) {
-
-		return new ResponseEntity<>(null, HttpStatus.OK);
+	public ResponseEntity<AirlineRule> putAirlineRules(@Valid @RequestBody AirlineRule airlineRule) {
+		return new ResponseEntity<>(service.save(airlineRule), HttpStatus.OK);
 	}
-	
-	@DeleteMapping(path = "/airline-rules")
+
+	@DeleteMapping(path = "/airline-rules/{airlineCode}")
 	@ResponseBody
 	@ApiOperation(value = "Remove airline rules")
 	@Internal
-	public ResponseEntity<?> removeAirlineRules(Supplier<?> supplier) {
-
-		return new ResponseEntity<>(null, HttpStatus.OK);
+	public ResponseEntity<String> removeAirlineRules(@PathVariable String airlineCode) {
+		return new ResponseEntity<>(service.delete(airlineCode), HttpStatus.OK);
 	}
 
 }
