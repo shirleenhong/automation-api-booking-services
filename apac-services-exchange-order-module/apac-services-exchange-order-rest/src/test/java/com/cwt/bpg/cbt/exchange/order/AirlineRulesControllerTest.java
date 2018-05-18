208d60e29a17f5df7eea3eb7c5a54fcd146164e6
puigterence <puigterence@carlsonwagonlit.com>
2018-05-18 10:54:07 +0800

    fix unit test

--- apac-services-exchange-order-module/apac-services-exchange-order-rest/src/test/java/com/cwt/bpg/cbt/exchange/order/AirlineRulesControllerTest.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-rest/src/test/java/com/cwt/bpg/cbt/exchange/order/AirlineRulesControllerTest.java
@@ -1,7 +1,11 @@
 package com.cwt.bpg.cbt.exchange.order;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,13 +18,13 @@ import org.springframework.http.ResponseEntity;
 import com.cwt.bpg.cbt.exchange.order.model.AirlineRule;
 
 public class AirlineRulesControllerTest {
-	
+
 	@Mock
 	private AirlineRuleService service;
-	
+
 	@InjectMocks
 	private AirlineRulesController airlineRulesController;
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
@@ -28,20 +32,32 @@ public class AirlineRulesControllerTest {
 
 	@Test
 	public void canGetAirlineRules() {
+		when(service.getAll()).thenReturn(new ArrayList<AirlineRule>());
+		
 		ResponseEntity<?> airlineRules = airlineRulesController.getAirlineRules();
-		assertEquals(HttpStatus.OK,  airlineRules.getStatusCode());
+		
+		assertEquals(HttpStatus.OK, airlineRules.getStatusCode());
+		verify(service, times(1)).getAll();
 	}
-	
+
 	@Test
 	public void canPutAirlineRules() {
 		final AirlineRule airlineRule = new AirlineRule();
+		when(service.save(airlineRule)).thenReturn(airlineRule);
+
 		ResponseEntity<?> client = airlineRulesController.putAirlineRules(airlineRule);
+
 		assertEquals(HttpStatus.OK, client.getStatusCode());
+		verify(service, times(1)).save(airlineRule);
 	}
-	
+
 	@Test
 	public void canRemoveAirlineRules() {
-		ResponseEntity<?> client = airlineRulesController.removeAirlineRules("CAT1");
+		final String airlineCode = "A1";
+		when(service.delete(airlineCode)).thenReturn(airlineCode);
+		
+		ResponseEntity<?> client = airlineRulesController.removeAirlineRules(airlineCode);
 		assertEquals(HttpStatus.OK, client.getStatusCode());
+		verify(service, times(1)).delete(airlineCode);
 	}
 }
