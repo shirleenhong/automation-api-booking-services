1685393c46d4a846f600c44adaaef7af60e66bae
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-05-18 11:26:34 +0800

    Unit Test OtherServiceFeesController for /air-fees and /nett-cost

--- apac-services-exchange-order-module/apac-services-exchange-order-rest/src/test/java/com/cwt/bpg/cbt/exchange/order/OtherServiceFeesControllerAirFeeTest.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-rest/src/test/java/com/cwt/bpg/cbt/exchange/order/OtherServiceFeesControllerAirFeeTest.java
@@ -1,54 +1,90 @@
 package com.cwt.bpg.cbt.exchange.order;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
+import com.cwt.bpg.cbt.exchange.order.model.AirFeesInput;
+import net.minidev.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.cwt.bpg.cbt.exchange.order.model.AirFeesBreakdown;
-import com.cwt.bpg.cbt.exchange.order.model.AirFeesInput;
-import com.cwt.bpg.cbt.exchange.order.model.VisaFeesBreakdown;
-import com.cwt.bpg.cbt.exchange.order.model.VisaFeesInput;
+import java.nio.charset.Charset;
 
+import static com.cwt.bpg.cbt.exchange.order.OtherServiceFeesControllerMiscFeeTest.convertObjectToJsonBytes;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 public class OtherServiceFeesControllerAirFeeTest {
 
-	
-	@Mock
-	private OtherServiceFeesService service;
-	
-	@InjectMocks
-	private OtherServiceFeesController controller;
-	
-	
-	@Before
-	public void init() {
-		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Test
-	public void canComputeAirFees() {
-		AirFeesInput input = new AirFeesInput();
-		
-		AirFeesBreakdown value = mock(AirFeesBreakdown.class);
-		when(service.calculateAirFee(input)).thenReturn(value);
-		
-		ResponseEntity<AirFeesBreakdown> computeAirFees = controller.computeAirFees(input);		
-		verify(service, times(1)).calculateAirFee(input);
-		assertEquals(HttpStatus.OK, computeAirFees.getStatusCode());
-	}
-	
-	@Test
-	public void canComputeVisaFees() {
-		VisaFeesInput input = new VisaFeesInput();
-		ResponseEntity<VisaFeesBreakdown> computeVisaFee = controller.computeVisaFee(input);
-		
-		assertEquals(HttpStatus.OK, computeVisaFee.getStatusCode());
-	}
+    private MockMvc mockMvc;
+
+    @Mock
+    private OtherServiceFeesService service;
+
+    @InjectMocks
+    private OtherServiceFeesController controller;
+
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8")
+    );
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Test
+    public void shouldReturnFeesBreakdown() throws Exception {
+        JSONObject jsonObj = new JSONObject();
+
+        jsonObj.put("clientType", "Z");
+        jsonObj.put("profileName", "testName");
+        jsonObj.put("fopType", "Z");
+        jsonObj.put("applyFormula", 111);
+        jsonObj.put("commissionByPercent", 111);
+
+        MockHttpServletResponse response = mockMvc.perform(post("/other-service-fees/air-fees")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(convertObjectToJsonBytes(jsonObj)))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        verify(service, times(1)).calculateAirFee(any(AirFeesInput.class));
+    }
+
+    @Test
+    public void shouldReturnBadRequestOnEmptyMandatoryField() throws Exception {
+        JSONObject jsonObj = new JSONObject();
+
+        jsonObj.put("clientType", "");
+        jsonObj.put("profileName", "testName");
+        jsonObj.put("fopType", "Z");
+        jsonObj.put("applyFormula", 111);
+        jsonObj.put("commissionByPercent", 111);
+
+
+        mockMvc.perform(post("/other-service-fees/air-fees")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(convertObjectToJsonBytes(jsonObj)))
+                .andExpect(status().isBadRequest());
+
+        verifyZeroInteractions(service);
+    }
 
 }
