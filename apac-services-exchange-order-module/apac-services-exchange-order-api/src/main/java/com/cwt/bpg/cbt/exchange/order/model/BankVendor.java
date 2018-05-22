95cad5fa3f825829b323fd3e6a06e76fec5eac2c
puigterence <puigterence@carlsonwagonlit.com>
2018-05-22 18:01:18 +0800

    added client pricing model

---
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/BankVendor.java
@@ -0,0 +1,43 @@
+package com.cwt.bpg.cbt.exchange.order.model;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = { "clientId" })
+public class BankVendor implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int clientId;
+	private String vendorName;
+	private Double percentage;
+	private boolean standard;
+	
+	public int getClientId() {
+		return clientId;
+	}
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+	public String getVendorName() {
+		return vendorName;
+	}
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
+	}
+	public Double getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(Double percentage) {
+		this.percentage = percentage;
+	}
+	public boolean isStandard() {
+		return standard;
+	}
+	public void setStandard(boolean standard) {
+		this.standard = standard;
+	}
+	
+	
+}
--- apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/Client.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/Client.java
@@ -1,9 +1,100 @@
 package com.cwt.bpg.cbt.exchange.order.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Client implements Serializable {
 
-	private static final long serialVersionUID = 6596806910810074104L;
+	private static final long serialVersionUID = 4416580993384869665L;
+	
+	private int clientId;
+	private int cmpid;
+	private String name;
+	private String profileName;
+	private Integer pricingId;
+	private Boolean exemptTax;
+	private List<ProductMerchantFee> products;
+	private List<BankVendor> vendors;
+	private List<Bank> banks;
+	private List<ClientPricing> clientPricings;
 
+	public void setProducts(List<ProductMerchantFee> list) {
+		this.products = list;
+	}
+
+	public void setVendors(List<BankVendor> list) {
+		this.vendors = list;
+	}
+
+	public void setBanks(List<Bank> list) {
+		this.banks = list;
+	}
+
+	public List<ProductMerchantFee> getProducts() {
+		return products;
+	}
+
+	public List<BankVendor> getVendors() {
+		return vendors;
+	}
+
+	public List<Bank> getBanks() {
+		return banks;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	public int getCmpid() {
+		return cmpid;
+	}
+
+	public void setCmpid(int cmpid) {
+		this.cmpid = cmpid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public Integer getPricingId() {
+		return pricingId;
+	}
+
+	public void setPricingId(Integer pricingId) {
+		this.pricingId = pricingId;
+	}
+
+	public Boolean getExemptTax() {
+		return exemptTax;
+	}
+
+	public void setExemptTax(Boolean exemptTax) {
+		this.exemptTax = exemptTax;
+	}
+
+	public List<ClientPricing> getClientPricings() {
+		return clientPricings;
+	}
+
+	public void setClientPricings(List<ClientPricing> clientPricings) {
+		this.clientPricings = clientPricings;
+	}
 }
