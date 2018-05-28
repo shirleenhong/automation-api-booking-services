5f2656a19f998fc898d31313bc5f03c4b0f77439
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-05-28 12:03:38 +0800

    [TA3563][TASK: Create No Fee Calculator] Add overriden methods to NoFeeCalculator and created NoFeeCalculatorTest

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/NoFeeCalculator.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/NoFeeCalculator.java
@@ -1,24 +1,40 @@
 package com.cwt.bpg.cbt.exchange.order.calculator.tf;
 
 public class NoFeeCalculator extends TransactionFeeCalculator {
-    public Double getTotalDiscount(){return null;}
-    public Double getTotalORCom(){return null;}
-    public Double getTotalORCom2(){return null;}
-    public Double getMFFee(Double totalSellFare, Double totalTaxes, Double totalGST, Double merchantFee){
-        //({Total Sell Fare}+{Total Taxes}+{Total GST})*{Merchant Fee %}
-        return (totalSellFare+totalTaxes+totalGST)*merchantFee;
+    public Double getTotalDiscount() {
+        return null;
     }
 
-    public Double getMFFeeTF(){return null;}
-    public Double getDDLFeeApply(){/*TODO return NA; what is NA?*/return null;}
-    public Double getTotalFee(){return null;}
-    public Double getTotalSellingFare(Double totalSellFare,Double totalGST,Double totalMerchantFee){
-        //{Total Sell Fare}+{Total GST}+{Total Merchant Fee}
-        return totalSellFare+totalGST+totalMerchantFee;
+    public Double getTotalORCom() {
+        return null;
     }
-    public Double getTotalCharge(Double totalSellFare,Double totalGST,Double totalMerchantFee, Double totalTaxes){
-        //{Total Sell Fare}+{Total GST}+{Total Merchant Fee}+{Total Taxes}
-        return totalSellFare+totalGST+totalMerchantFee+totalTaxes;
+
+    public Double getTotalORCom2() {
+        return null;
+    }
+
+    public Double getMFFee(Double totalSellFare, Double totalTaxes, Double totalGST, Double merchantFee) {
+        return (totalSellFare + totalTaxes + totalGST) * merchantFee;
+    }
+
+    public Double getMFFeeTF() {
+        return null;
+    }
+
+    public Double getDDLFeeApply() {
+        return null;
+    }
+
+    public Double getTotalFee() {
+        return null;
+    }
+
+    public Double getTotalSellingFare(Double totalSellFare, Double totalGST, Double totalMerchantFee) {
+        return totalSellFare + totalGST + totalMerchantFee;
+    }
+
+    public Double getTotalCharge(Double totalSellFare, Double totalGST, Double totalMerchantFee, Double totalTaxes) {
+        return totalSellFare + totalGST + totalMerchantFee + totalTaxes;
     }
 
 }
