799e6975b69f1e98d415e04946ceacde59c70ce5
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-05-28 14:14:39 +0800

    [TA3565][TASK: Create TransactionFeeRebate  Calculator] Code review revisions
    Signed-off-by: alpatricio <annajosedel.patricio@carlsonwagonlit.com>

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/RebateCalculator.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/RebateCalculator.java
@@ -2,12 +2,17 @@ package com.cwt.bpg.cbt.exchange.order.calculator.tf;
 
 public class RebateCalculator extends TransactionFeeCalculator {
 
-    public Double getIntMfFeeTF(){ return null; }
+    public Double getIntMfFeeTF() {
+        return null;
+    }
 
-    public Double getIntDdlFeeApply(){ return null; }
+    public Double getIntDdlFeeApply() {
+        return null;
+    }
 
     public Double getIntTotalCharge(Double totalSellFare, Double totalDiscount,
-           Double totalGST, Double totalMerchantFee, Double fee, Double totalTaxes){
-        return totalSellFare-totalDiscount+totalGST+totalMerchantFee+fee+totalTaxes;
+                                    Double totalGST, Double totalMerchantFee, Double fee, Double totalTaxes) {
+        return safeValue(totalSellFare) - safeValue(totalDiscount) + safeValue(totalGST) +
+                safeValue(totalMerchantFee) + safeValue(fee) + safeValue(totalTaxes);
     }
 }
