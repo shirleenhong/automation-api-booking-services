6d6720a41962da2ac01591f531dbf673b23d7169
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-05-28 14:19:03 +0800

    [TA3565][TASK: Create TransactionFeeRebate  Calculator] Code review revisions
    Signed-off-by: alpatricio <annajosedel.patricio@carlsonwagonlit.com>

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/RebateCalculatorTest.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/RebateCalculatorTest.java
@@ -3,6 +3,7 @@ package com.cwt.bpg.cbt.exchange.order.calculator.tf;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class RebateCalculatorTest {
@@ -20,6 +21,17 @@ public class RebateCalculatorTest {
 
     @Test
     public void getIntTotalChargeShouldReturnNotNull() {
-        assertNotNull(rebateCalc.getIntTotalCharge(1d,1d,1d,1d,1d,1d));
+        Double totalSellFare = 1d;
+        Double totalDiscount = 1d;
+        Double totalGST = 1d;
+        Double totalMerchantFee = 1d;
+        Double fee = 1d;
+        Double totalTaxes = 1d;
+
+        Double expectedResult = totalSellFare - totalDiscount + totalGST + totalMerchantFee + fee + totalTaxes;
+        Double actualResult = rebateCalc.getIntTotalCharge(1d, 1d, 1d, 1d, 1d, 1d);
+
+        assertNotNull(actualResult);
+        assertEquals(actualResult, expectedResult);
     }
 }
