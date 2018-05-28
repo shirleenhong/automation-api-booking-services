dcf31e9ef8398e9f3196131ecf7340a41d6cb25d
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-05-28 15:27:58 +0800

    Code review revisions : Use Camelcase
    Signed-off-by: alpatricio <annajosedel.patricio@carlsonwagonlit.com>

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/RebateCalculator.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/RebateCalculator.java
@@ -13,8 +13,8 @@ public class RebateCalculator extends TransactionFeeCalculator {
     }
 
     public BigDecimal getTotalCharge(BigDecimal totalSellFare, BigDecimal totalDiscount,
-                                     BigDecimal totalGST, BigDecimal totalMerchantFee, BigDecimal fee, BigDecimal totalTaxes) {
-        return safeValue(totalSellFare).subtract(safeValue(totalDiscount)).add(safeValue(totalGST))
+                                     BigDecimal totalGst, BigDecimal totalMerchantFee, BigDecimal fee, BigDecimal totalTaxes) {
+        return safeValue(totalSellFare).subtract(safeValue(totalDiscount)).add(safeValue(totalGst))
                 .add(safeValue(totalMerchantFee)).add(safeValue(fee)).add(safeValue(totalTaxes));
     }
 }
