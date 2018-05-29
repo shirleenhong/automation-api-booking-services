d2788367da361e8377964445c76d5fafc815cc88
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-05-29 09:50:18 +0800

    [TA3564][TASK: Create TransactionFeeVAT Calculator] Override WithVatCalculator methods. Create WithVatCalculatorTest.
    Signed-off-by: alpatricio <annajosedel.patricio@carlsonwagonlit.com>

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/WithVatCalculator.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/main/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/WithVatCalculator.java
@@ -3,10 +3,10 @@ package com.cwt.bpg.cbt.exchange.order.calculator.tf;
 import java.math.BigDecimal;
 
 public class WithVatCalculator extends TransactionFeeCalculator {
-    public BigDecimal getTotalFee(BigDecimal baseFare, BigDecimal totalTaxes, BigDecimal totalGst,
-                                  BigDecimal totalAirlineCommission, BigDecimal totalReturnableOr) {
+	public BigDecimal getTotalFee(BigDecimal baseFare, BigDecimal totalTaxes, BigDecimal totalGst,
+			BigDecimal totalAirlineCommission, BigDecimal totalReturnableOr) {
 
-        return safeValue(baseFare).add(safeValue(totalTaxes)).add(safeValue(totalGst))
-                .subtract(safeValue(totalAirlineCommission)).subtract(totalReturnableOr);
-    }
+		return safeValue(baseFare).add(safeValue(totalTaxes)).add(safeValue(totalGst))
+				.subtract(safeValue(totalAirlineCommission)).subtract(totalReturnableOr);
+	}
 }
--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/WithVatCalculatorTest.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/calculator/tf/WithVatCalculatorTest.java
@@ -8,24 +8,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class WithVatCalculatorTest {
-    private WithVatCalculator withVatCalculator = new WithVatCalculator();
+	private WithVatCalculator withVatCalculator = new WithVatCalculator();
 
-    @Test
-    public void getTotalFeeShouldReturn() {
+	@Test
+	public void getTotalFeeShouldReturn() {
 
-        BigDecimal baseFare = new BigDecimal(1),
-                totalTaxes = new BigDecimal(1),
-                totalGst = new BigDecimal(1),
-                totalAirlineCommission = new BigDecimal(1),
-                totalReturnableOr = new BigDecimal(1);
+		BigDecimal baseFare = new BigDecimal(1), totalTaxes = new BigDecimal(1), totalGst = new BigDecimal(1),
+				totalAirlineCommission = new BigDecimal(1), totalReturnableOr = new BigDecimal(1);
 
-        BigDecimal expectedResult = baseFare.add(totalTaxes).add(totalGst)
-                .subtract(totalAirlineCommission).subtract(totalReturnableOr);
+		BigDecimal expectedResult = baseFare.add(totalTaxes).add(totalGst).subtract(totalAirlineCommission)
+				.subtract(totalReturnableOr);
 
-        BigDecimal actualResult = withVatCalculator.getTotalFee(baseFare, totalTaxes, totalGst,
-                totalAirlineCommission, totalReturnableOr);
+		BigDecimal actualResult = withVatCalculator
+				.getTotalFee(baseFare, totalTaxes, totalGst, totalAirlineCommission, totalReturnableOr);
 
-        assertNotNull(actualResult);
-        assertEquals(expectedResult, actualResult);
-    }
+		assertNotNull(actualResult);
+		assertEquals(expectedResult, actualResult);
+	}
 }
