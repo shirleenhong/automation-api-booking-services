574dd1c7112649c94361b019881c0910d5ca2d30
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-06-14 16:06:29 +0800

    [US687][TA4495] TASK: Create RemarkList and Remark model

--- apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/Remark.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/Remark.java
@@ -1,7 +1,44 @@
 package com.cwt.bpg.cbt.exchange.order.model;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import org.mongodb.morphia.annotations.Entity;
+
 import java.io.Serializable;
 
+@Entity(value = "remarks", noClassnameStored = true)
 public class Remark implements Serializable {
     private static final long serialVersionUID = -756695804842165089L;
+
+    @NotEmpty
+    private String productType;
+
+    @NotEmpty
+    private String remarkType;
+
+    @NotEmpty
+    private String text;
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getRemarkType() {
+        return remarkType;
+    }
+
+    public void setRemarkType(String remarkType) {
+        this.remarkType = remarkType;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
