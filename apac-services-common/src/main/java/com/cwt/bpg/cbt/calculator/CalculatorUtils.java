b0c6677c611737c8e50c31f8b9a0b0e08a666bda
bobbyperez-cwt <bperez2@carlsonwagonlit.com>
2018-06-18 12:25:49 +0800

    makd CalculatorUtils final
    Signed-off-by: bobbyperez-cwt <bperez2@carlsonwagonlit.com>

--- apac-services-common/src/main/java/com/cwt/bpg/cbt/calculator/CalculatorUtils.java
+++ apac-services-common/src/main/java/com/cwt/bpg/cbt/calculator/CalculatorUtils.java
@@ -3,17 +3,15 @@ package com.cwt.bpg.cbt.calculator;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class CalculatorUtils
-{
+public final class CalculatorUtils {
 
-    private CalculatorUtils()
-    {
-        throw new IllegalStateException("utility class");
-    }
+	private CalculatorUtils() {
+		throw new IllegalStateException("utility class");
+	}
 
-    public static String safeValue(String value) {
-        return value == null ? "" : value;
-    }
+	public static String safeValue(String value) {
+		return value == null ? "" : value;
+	}
 
 	public static BigDecimal safeValue(BigDecimal value) {
 		return value == null ? BigDecimal.ZERO : value;
@@ -23,18 +21,12 @@ public class CalculatorUtils
 		return value == null ? 0D : value;
 	}
 
-    public static int safeValue(Integer value) {
-        if (value == null) {
-            return 0;
-        }
-        return value;
-    }
+	public static int safeValue(Integer value) {
+		return value == null ? 0 : value;
+	}
 
 	public static BigDecimal round(BigDecimal amount, int scale) {
-		if (amount == null) {
-			return null;
-		}
-		return amount.setScale(scale, RoundingMode.HALF_UP);
+		return amount == null ? null : amount.setScale(scale, RoundingMode.HALF_UP);
 	}
 
 	public static BigDecimal calculatePercentage(BigDecimal input, Double percent) {
