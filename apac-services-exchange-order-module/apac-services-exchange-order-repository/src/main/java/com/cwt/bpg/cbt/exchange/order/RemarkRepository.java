537ef4abf4a395117d4de3238858aaa21825263c
bobbyperez-cwt <bperez2@carlsonwagonlit.com>
2018-06-22 19:45:21 +0800

    resolved warnings
    Signed-off-by: bobbyperez-cwt <bperez2@carlsonwagonlit.com>

--- apac-services-exchange-order-module/apac-services-exchange-order-repository/src/main/java/com/cwt/bpg/cbt/exchange/order/RemarkRepository.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-repository/src/main/java/com/cwt/bpg/cbt/exchange/order/RemarkRepository.java
@@ -1,30 +1,29 @@
 package com.cwt.bpg.cbt.exchange.order;
 
-import com.cwt.bpg.cbt.exchange.order.model.Remark;
+import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.cwt.bpg.cbt.exchange.order.model.Remark;
 
 @Repository
 public class RemarkRepository extends CommonRepository<Remark, ObjectId>{
-    public static final String KEY_COLUMN = "id";
+    private static final String KEY_COLUMN = "id";
 
     public RemarkRepository() {
         super(Remark.class, KEY_COLUMN);
     }
 
     public List<Remark> getRemarks(String countryCode, String productType, String remarkType) {
-        List<Remark> remarks =  morphia.getDatastore().createQuery(Remark.class)
-        		.order("text")
+        return morphia.getDatastore().createQuery(Remark.class)
                 .field("countryCode")
                 .equal(countryCode)
                 .field("productType")
                 .equal(productType)
                 .field("remarkType")
-                .equal(remarkType).asList();
-        return remarks;
+                .equal(remarkType)
+                .order("text").asList();
     }
 
 }
