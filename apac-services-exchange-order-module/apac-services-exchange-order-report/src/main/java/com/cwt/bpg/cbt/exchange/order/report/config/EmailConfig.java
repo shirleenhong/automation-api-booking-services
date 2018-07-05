14532b1a40a6ee2b0060f5280a033a6db86717c8
Mary Grace Tagapan <marygrace.tagapan@carlsonwagonlit.com>
2018-07-05 14:27:35 +0800

    Email improvements

---
+++ apac-services-exchange-order-module/apac-services-exchange-order-report/src/main/java/com/cwt/bpg/cbt/exchange/order/report/config/EmailConfig.java
@@ -0,0 +1,47 @@
+package com.cwt.bpg.cbt.exchange.order.report.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration("com.cwt.bpg.cbt.email.config")
+public class EmailConfig {
+	
+	@Value("${exchange.order.mail.host}")
+	private String host;
+	
+	@Value("${exchange.order.mail.port}")
+	private int port;
+	
+	@Value("${exchange.order.mail.transport.protocol}")
+	private String transportProtocol;
+	
+	@Value("${exchange.order.mail.smtp.auth}")
+	private String smtpAuth;
+	
+	@Value("${exchange.order.mail.smtp.starttls.enable}")
+	private String enableStarttls;
+	
+	@Value("${exchange.order.mail.debug}")
+	private String mailDebug;
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost(host);
+	    mailSender.setPort(port);
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", transportProtocol);
+	    props.put("mail.smtp.auth", smtpAuth);
+	    props.put("mail.smtp.starttls.enable", enableStarttls);
+	    props.put("mail.debug",mailDebug);
+	     
+	    return mailSender;
+	}
+	
+}
