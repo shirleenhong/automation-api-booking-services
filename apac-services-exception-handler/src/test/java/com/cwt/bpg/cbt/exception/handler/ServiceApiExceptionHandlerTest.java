be881ee233b73f062c10f7ab20c93313cd6fc93c
Jay-cwt <Ahmad.Ali@carlsonwagonlit.com>
2018-07-05 12:10:04 +0800

    Completed the unit tests for ServiceApiExceptionHandler.

--- apac-services-exception-handler/src/test/java/com/cwt/bpg/cbt/exception/handler/ServiceApiExceptionHandlerTest.java
+++ apac-services-exception-handler/src/test/java/com/cwt/bpg/cbt/exception/handler/ServiceApiExceptionHandlerTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
@@ -14,6 +18,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.io.IOException;
 
 public class ServiceApiExceptionHandlerTest {
 
@@ -40,6 +46,54 @@ public class ServiceApiExceptionHandlerTest {
 		assertEquals(errorMessage, error.getMessage());
 		assertEquals(HttpStatus.BAD_REQUEST, error.getStatus());
 		assertTrue(error.getErrors().size() == 1);
+	}
+
+	@Test
+	public void canHandleInvalidJson() throws IOException {
+
+		String invalidJson = "{ \"fieldName\" : }";
+		String jsonErrorMessage = "Invalid JSON";
+		JsonParser parser  = new JsonFactory().createParser(invalidJson);
+		JsonParseException jsonEx = new JsonParseException(parser,
+				jsonErrorMessage);
+
+		String errorMessage = "Invalid Input";
+		HttpMessageNotReadableException ex = new HttpMessageNotReadableException(
+				errorMessage, jsonEx);
+
+		HttpHeaders headers = Mockito.mock(HttpHeaders.class);
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		WebRequest request = Mockito.mock(WebRequest.class);
+
+		ResponseEntity<Object> handleHttpMessageNotReadable = exceptionHandler
+				.handleHttpMessageNotReadable(ex, headers, status, request);
+
+		assertNotNull(handleHttpMessageNotReadable);
+		assertTrue(handleHttpMessageNotReadable.getBody() instanceof ApiError);
+	}
+
+	@Test
+	public void canHandleJsonInvalidValue() throws IOException {
+
+		String invalidJson = "{ \"countryCode\" : 1 }";
+		String formatErrorMessage = "Invalid Value";
+		JsonParser parser  = new JsonFactory().createParser(invalidJson);
+		InvalidFormatException ifx = new InvalidFormatException(parser,
+				formatErrorMessage, 1, String.class);
+
+		String errorMessage = "Invalid Input";
+		HttpMessageNotReadableException ex = new HttpMessageNotReadableException(
+				errorMessage, ifx);
+
+		HttpHeaders headers = Mockito.mock(HttpHeaders.class);
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		WebRequest request = Mockito.mock(WebRequest.class);
+
+		ResponseEntity<Object> handleHttpMessageNotReadable = exceptionHandler
+				.handleHttpMessageNotReadable(ex, headers, status, request);
+
+		assertNotNull(handleHttpMessageNotReadable);
+		assertTrue(handleHttpMessageNotReadable.getBody() instanceof ApiError);
 	}
 
 	@Test
@@ -70,6 +124,16 @@ public class ServiceApiExceptionHandlerTest {
 
 		assertNotNull(handleHttpMessageNotReadable);
 		assertTrue(handleHttpMessageNotReadable.getBody() instanceof ApiError);
+	}
+
+	@Test
+	public void canHandleServerError() {
+		NullPointerException ex = new NullPointerException("Null pointer");
+
+		ResponseEntity<Object> handleInternalServerError = exceptionHandler.handleInternalServerError(ex);
+
+		assertNotNull(handleInternalServerError);
+		assertTrue(handleInternalServerError.getBody() instanceof ApiError);
 	}
 
 	static class DummyType {
