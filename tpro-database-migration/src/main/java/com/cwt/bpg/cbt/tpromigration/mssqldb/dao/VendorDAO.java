6e26b1ae9d5c36f3dda0015c570479917f805f2b
darwin <DarwinJames.Abelardo@carlsonwagonlit.com>
2018-07-16 12:12:35 +0800

    US783 - Re-migrate Products and Vendor with contactInfo

--- tpro-database-migration/src/main/java/com/cwt/bpg/cbt/tpromigration/mssqldb/dao/VendorDAO.java
+++ tpro-database-migration/src/main/java/com/cwt/bpg/cbt/tpromigration/mssqldb/dao/VendorDAO.java
@@ -2,9 +2,12 @@ package com.cwt.bpg.cbt.tpromigration.mssqldb.dao;
 
 import java.util.List;
 
+import com.cwt.bpg.cbt.exchange.order.model.ContactInfo;
 import com.cwt.bpg.cbt.tpromigration.mssqldb.model.Vendor;
 
 public interface VendorDAO {
 
 	List<Vendor> listVendors();
+
+	List<ContactInfo> listVendorContactInfo();
 }
