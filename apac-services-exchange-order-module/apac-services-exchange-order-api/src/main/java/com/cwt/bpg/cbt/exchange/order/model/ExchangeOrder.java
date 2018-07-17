67cf6fba2972c68e87ebe03df700d2d68ae2dda6
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-07-17 14:27:13 +0800

    [US741][TA5823] TASK: Apply caching

--- apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/ExchangeOrder.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/ExchangeOrder.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.annotations.ApiModelProperty;
 
 @Entity(value = "exchangeOrderTransactions", noClassnameStored = true)
-@Indexes(@Index(fields = @Field("eoNumber")))
+@Indexes(@Index(fields = {@Field("eoNumber"),@Field("recordLocator")}))
 public class ExchangeOrder implements Serializable {
 
 	private static final long serialVersionUID = 79442657760597469L;
