e739f15f50ba2b9b761147b87eec7a2922606ba0
Jay-cwt <Ahmad.Ali@carlsonwagonlit.com>
2018-07-26 17:50:01 +0800

    Use CreditCard model on FormOfPayment model.

--- apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/FormOfPayment.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/FormOfPayment.java
@@ -1,60 +1,28 @@
 package com.cwt.bpg.cbt.exchange.order.model;
 
-import io.swagger.annotations.ApiModelProperty;
-import org.hibernate.validator.constraints.NotEmpty;
-
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 public class FormOfPayment implements Serializable {
 
     private static final long serialVersionUID = 1850641277169808339L;
 
-    @NotNull
-    @ApiModelProperty(required = true)
 	private FopTypes fopType;
 
-    @NotEmpty
-    @ApiModelProperty(required = true)
-    private String ccType;
+    private CreditCard creditCard;
 
-    @NotEmpty
-    @ApiModelProperty(required = true)
-    private String ccNumber;
+	public FopTypes getFopType() {
+		return fopType;
+	}
 
-    @NotEmpty
-    @ApiModelProperty(required = true)
-    private String expiryDate;
+	public void setFopType(FopTypes fopType) {
+		this.fopType = fopType;
+	}
 
-    public FopTypes getFopType() {
-        return fopType;
-    }
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
 
-    public void setFopType(FopTypes fopType) {
-        this.fopType = fopType;
-    }
-
-    public String getCcType() {
-        return ccType;
-    }
-
-    public void setCcType(String ccType) {
-        this.ccType = ccType;
-    }
-
-    public String getCcNumber() {
-        return ccNumber;
-    }
-
-    public void setCcNumber(String ccNumber) {
-        this.ccNumber = ccNumber;
-    }
-
-    public String getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(String expiryDate) {
-        this.expiryDate = expiryDate;
-    }
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
 }
