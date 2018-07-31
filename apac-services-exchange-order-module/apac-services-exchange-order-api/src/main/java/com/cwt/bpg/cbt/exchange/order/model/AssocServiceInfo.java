1e7046464607cec20e72a2217bfc950409809501
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2018-07-31 16:51:19 +0800

    [US846][TA6995] TASK: Create India Assoc Service Info submodel

---
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/AssocServiceInfo.java
@@ -0,0 +1,149 @@
+package com.cwt.bpg.cbt.exchange.order.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class AssocServiceInfo implements Serializable {
+
+    private static final long serialVersionUID = -7551325200425808468L;
+
+    private String details;
+
+    private String productCode;
+
+    private String vendorCode;
+
+    private FormOfPayment formOfPayment;
+
+    private BigDecimal nettCost;
+
+    private BigDecimal commission;
+
+    private BigDecimal discount;
+
+    private BigDecimal sellingPrice;
+
+    private BigDecimal vatGstAmount;
+
+    private BigDecimal merchantFeeAmount;
+
+    private String productDescription;
+
+    private String cwtRefNoTk;
+
+    private String vendorRefNoGsa;
+
+    private String otherRelatedNoPo;
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getVendorCode() {
+        return vendorCode;
+    }
+
+    public void setVendorCode(String vendorCode) {
+        this.vendorCode = vendorCode;
+    }
+
+    public FormOfPayment getFormOfPayment() {
+        return formOfPayment;
+    }
+
+    public void setFormOfPayment(FormOfPayment formOfPayment) {
+        this.formOfPayment = formOfPayment;
+    }
+
+    public BigDecimal getNettCost() {
+        return nettCost;
+    }
+
+    public void setNettCost(BigDecimal nettCost) {
+        this.nettCost = nettCost;
+    }
+
+    public BigDecimal getCommission() {
+        return commission;
+    }
+
+    public void setCommission(BigDecimal commission) {
+        this.commission = commission;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public BigDecimal getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(BigDecimal sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public BigDecimal getVatGstAmount() {
+        return vatGstAmount;
+    }
+
+    public void setVatGstAmount(BigDecimal vatGstAmount) {
+        this.vatGstAmount = vatGstAmount;
+    }
+
+    public BigDecimal getMerchantFeeAmount() {
+        return merchantFeeAmount;
+    }
+
+    public void setMerchantFeeAmount(BigDecimal merchantFeeAmount) {
+        this.merchantFeeAmount = merchantFeeAmount;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getCwtRefNoTk() {
+        return cwtRefNoTk;
+    }
+
+    public void setCwtRefNoTk(String cwtRefNoTk) {
+        this.cwtRefNoTk = cwtRefNoTk;
+    }
+
+    public String getVendorRefNoGsa() {
+        return vendorRefNoGsa;
+    }
+
+    public void setVendorRefNoGsa(String vendorRefNoGsa) {
+        this.vendorRefNoGsa = vendorRefNoGsa;
+    }
+
+    public String getOtherRelatedNoPo() {
+        return otherRelatedNoPo;
+    }
+
+    public void setOtherRelatedNoPo(String otherRelatedNoPo) {
+        this.otherRelatedNoPo = otherRelatedNoPo;
+    }
+}
