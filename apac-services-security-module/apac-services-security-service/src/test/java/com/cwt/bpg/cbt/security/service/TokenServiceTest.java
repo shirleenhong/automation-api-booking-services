173f4a644893469a66c50c565cab1f4ec65e981b
bobby perez <bperez2@carlsonwagonlit.com>
2018-08-30 12:34:21 +0800

    [1.4] validate token secret - this required generation of new tokens

--- apac-services-security-module/apac-services-security-service/src/test/java/com/cwt/bpg/cbt/security/service/TokenServiceTest.java
+++ apac-services-security-module/apac-services-security-service/src/test/java/com/cwt/bpg/cbt/security/service/TokenServiceTest.java
@@ -1,19 +1,18 @@
 package com.cwt.bpg.cbt.security.service;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
-
-import java.io.IOException;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.cwt.bpg.cbt.security.api.model.Token;
 import com.cwt.bpg.cbt.security.repository.TokenRepository;
@@ -23,26 +22,31 @@ public class TokenServiceTest {
 
 	@InjectMocks
 	private TokenService service;
-	
+
 	@Mock
 	private TokenRepository repo;
-	
+
+	private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJUZXN0In0.KrQkG8lz8n4GaHl4uv2WAhdIKRwn_44riqcBKg9gP5w";
+
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
+		ReflectionTestUtils.setField(service, "secretKey", "test-secret");
 	}
-		
+
 	@Test
-	public void shouldReturnTrue() throws IOException {
-		
-		Mockito.when(repo.getToken(anyString())).thenReturn(new Token());
-		assertTrue(service.isTokenExist("tokenkey"));
+	public void shouldReturnTrue() {
+		Token token = new Token();
+		token.setKey(VALID_TOKEN);
+		when(repo.getToken(anyString())).thenReturn(token);
+
+		assertTrue(service.isTokenExist(VALID_TOKEN));
 	}
-	
+
 	@Test
-	public void shouldReturnFalse() throws IOException {
-		
-		Mockito.when(repo.getToken(anyString())).thenReturn(null);
-		assertFalse(service.isTokenExist("tokenkey"));
-	}	
+	public void shouldReturnFalse() {
+		when(repo.getToken(anyString())).thenReturn(null);
+
+		assertFalse(service.isTokenExist(null));
+	}
 }
