6e646fda4b89edc0b47ce38e17e4a834ad4f4f1e
darwin <DarwinJames.Abelardo@carlsonwagonlit.com>
2018-09-05 18:09:36 +0800

    [1.5.0][US987] Refactor Request model for India

---
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/india/Tour.java
@@ -0,0 +1,79 @@
+package com.cwt.bpg.cbt.exchange.order.model.india;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.cwt.bpg.cbt.exchange.order.model.deserializer.DateDeserializer;
+import com.cwt.bpg.cbt.exchange.order.model.serializer.DateSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class Tour implements Serializable {
+	
+	private static final long serialVersionUID = 1184885404521305510L;
+
+	@ApiModelProperty(value = "Date in UTC", example = "2008-05-29T14:09:000Z")
+	@JsonSerialize(using = DateSerializer.class)
+	@JsonDeserialize(using = DateDeserializer.class)
+	private Instant startDate;
+	
+	@ApiModelProperty(value = "Date in UTC", example = "2008-05-29T14:09:000Z")
+	@JsonSerialize(using = DateSerializer.class)
+	@JsonDeserialize(using = DateDeserializer.class)
+	private Instant endDate;
+
+	private String passenger;
+	private String city;
+	private String packageName;
+	private String details;
+
+	public Instant getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Instant startDate) {
+		this.startDate = startDate;
+	}
+
+	public Instant getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Instant endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getPassenger() {
+		return passenger;
+	}
+
+	public void setPassenger(String passenger) {
+		this.passenger = passenger;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+}
