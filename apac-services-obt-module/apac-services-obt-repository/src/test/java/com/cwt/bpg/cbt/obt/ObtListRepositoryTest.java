6fac9873ba47132aea5cecf3ea7d830a7881b0bc
Jay-cwt <Ahmad.Ali@carlsonwagonlit.com>
2018-09-06 15:50:15 +0800

    Updates for sonar coverage.

---
+++ apac-services-obt-module/apac-services-obt-repository/src/test/java/com/cwt/bpg/cbt/obt/ObtListRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.cwt.bpg.cbt.obt;
+
+import com.cwt.bpg.cbt.mongodb.config.MorphiaComponent;
+import com.cwt.bpg.cbt.obt.model.ObtList;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.FieldEnd;
+import org.mongodb.morphia.query.Query;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ObtListRepositoryTest {
+	
+	@Mock
+	private Datastore dataStore;
+
+	@Mock
+	private MorphiaComponent morphia;
+
+	@InjectMocks
+	private ObtListRepository repository;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		Mockito.when(morphia.getDatastore()).thenReturn(dataStore);
+	}
+
+	@Test
+	public void getShouldReturnObtList() {
+		Query query = Mockito.mock(Query.class);
+		FieldEnd fieldEnd = Mockito.mock(FieldEnd.class);
+		Mockito.when(dataStore.createQuery(ObtList.class)).thenReturn(query);
+		Mockito.when(query.field(Mockito.anyString())).thenReturn(fieldEnd);
+		Mockito.when(fieldEnd.equal(anyString())).thenReturn(query);
+
+		repository.get("IN");
+
+		verify(morphia, times(1)).getDatastore();
+		verify(dataStore, times(1)).createQuery(ObtList.class);
+	}
+
+}
