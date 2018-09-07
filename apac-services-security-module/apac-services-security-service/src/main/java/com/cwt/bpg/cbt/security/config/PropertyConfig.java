739793eb779e58c17839af6395e296bf47ff4b4d
bobby perez <bperez2@carlsonwagonlit.com>
2018-09-07 19:52:07 +0800

    added private constructor to PropertyConfig classes

--- apac-services-security-module/apac-services-security-service/src/main/java/com/cwt/bpg/cbt/security/config/PropertyConfig.java
+++ apac-services-security-module/apac-services-security-service/src/main/java/com/cwt/bpg/cbt/security/config/PropertyConfig.java
@@ -5,56 +5,50 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration("com.cwt.bpg.cbt.security.config.property")
-public class PropertyConfig
-{
-    
-    @Configuration("com.cwt.bpg.cbt.security.config.property.source")
-    @Profile("local")
-    @PropertySource(value = { "classpath:properties/security-local.properties" }, ignoreResourceNotFound = true)
-    static class LocalPropertyConfig
-    {
-    }
-    
-    @Configuration("com.cwt.bpg.cbt.security.config.property.source")
-    @Profile("dev")
-    @PropertySource(value = { "classpath:properties/security-dev.properties" }, ignoreResourceNotFound = true)
-    static class DevPropertyConfig
-    {
-    }
+public class PropertyConfig {
+	private PropertyConfig() {
+	}
 
-    @Configuration("com.cwt.bpg.cbt.security.config.property.source")
-    @Profile("int")
-    @PropertySource(value = { "classpath:properties/security-int.properties" }, ignoreResourceNotFound = true)
-    static class IntPropertyConfig
-    {
-    }
-    
-    @Configuration("com.cwt.bpg.cbt.security.config.property.source")
-    @Profile("sec")
-    @PropertySource(value = { "classpath:properties/security-sec.properties" }, ignoreResourceNotFound = true)
-    static class SecurityPropertyConfig
-    {
-    }
-    
-    @Configuration("com.cwt.bpg.cbt.security.config.property.source")
-    @Profile("perf")
-    @PropertySource(value = { "classpath:properties/security-perf.properties" }, ignoreResourceNotFound = true)
-    static class PerfPropertyConfig
-    {
-    }
+	@Configuration("com.cwt.bpg.cbt.security.config.property.source")
+	@Profile("local")
+	@PropertySource(value = { "classpath:properties/security-local.properties" }, ignoreResourceNotFound = true)
+	static class LocalPropertyConfig {
+	}
 
-    @Configuration("com.cwt.bpg.cbt.security.config.property.source")
-    @Profile("preprod")
-    @PropertySource(value = { "classpath:properties/security-preprod.properties" }, ignoreResourceNotFound = true)
-    static class PreprodPropertyConfig
-    {
-    }
-    
-    @Configuration("com.cwt.bpg.cbt.security.config.property.source")
-    @Profile("prod")
-    @PropertySource(value = { "classpath:properties/security-prod.properties" }, ignoreResourceNotFound = true)
-    static class ProdPropertyConfig
-    {
-    }
+	@Configuration("com.cwt.bpg.cbt.security.config.property.source")
+	@Profile("dev")
+	@PropertySource(value = { "classpath:properties/security-dev.properties" }, ignoreResourceNotFound = true)
+	static class DevPropertyConfig {
+	}
+
+	@Configuration("com.cwt.bpg.cbt.security.config.property.source")
+	@Profile("int")
+	@PropertySource(value = { "classpath:properties/security-int.properties" }, ignoreResourceNotFound = true)
+	static class IntPropertyConfig {
+	}
+
+	@Configuration("com.cwt.bpg.cbt.security.config.property.source")
+	@Profile("sec")
+	@PropertySource(value = { "classpath:properties/security-sec.properties" }, ignoreResourceNotFound = true)
+	static class SecurityPropertyConfig {
+	}
+
+	@Configuration("com.cwt.bpg.cbt.security.config.property.source")
+	@Profile("perf")
+	@PropertySource(value = { "classpath:properties/security-perf.properties" }, ignoreResourceNotFound = true)
+	static class PerfPropertyConfig {
+	}
+
+	@Configuration("com.cwt.bpg.cbt.security.config.property.source")
+	@Profile("preprod")
+	@PropertySource(value = { "classpath:properties/security-preprod.properties" }, ignoreResourceNotFound = true)
+	static class PreprodPropertyConfig {
+	}
+
+	@Configuration("com.cwt.bpg.cbt.security.config.property.source")
+	@Profile("prod")
+	@PropertySource(value = { "classpath:properties/security-prod.properties" }, ignoreResourceNotFound = true)
+	static class ProdPropertyConfig {
+	}
 
 }
