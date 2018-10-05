e7bfea4ba15b91900f1a5a72b932085dfe44c557
darwin <DarwinJames.Abelardo@carlsonwagonlit.com>
2018-10-05 16:20:48 +0800

    [US1073] Update unit tests

--- apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/ClientServiceTest.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-service/src/test/java/com/cwt/bpg/cbt/exchange/order/ClientServiceTest.java
@@ -1,8 +1,13 @@
 package com.cwt.bpg.cbt.exchange.order;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.cwt.bpg.cbt.exchange.order.model.Client;
+import com.cwt.bpg.cbt.exchange.order.model.ClientPricing;
 
 public class ClientServiceTest {
 	
@@ -62,6 +68,46 @@ public class ClientServiceTest {
 		when(repository.getClient(clientAccountNumber)).thenReturn(client);
 		Client result = service.getClient(clientAccountNumber);
 		assertNotNull(result);
+		verify(repository, times(1)).getClient(clientAccountNumber);
+	}
+	
+	@Test
+	public void shouldGetClientPricings() {
+		Client client = new Client();
+		List<ClientPricing> clientPricings = new ArrayList<>();
+		ClientPricing clientPricing = new ClientPricing();
+		String clientAccountNumber = "12345";
+		String tripType = "I";
+		
+		clientPricing.setCmpid(1);
+		clientPricing.setTripType(tripType);
+		clientPricings.add(clientPricing);
+		client.setClientPricings(clientPricings);
+		
+		when(repository.getClient(clientAccountNumber)).thenReturn(client);
+		List<ClientPricing> result = service.getClientPricings(clientAccountNumber, tripType);
+		
+		assertFalse(result.isEmpty());
+		verify(repository, times(1)).getClient(clientAccountNumber);
+	}
+	
+	@Test
+	public void shouldGetClientPricingsNotEqualTripType() {
+		Client client = new Client();
+		List<ClientPricing> clientPricings = new ArrayList<>();
+		ClientPricing clientPricing = new ClientPricing();
+		String clientAccountNumber = "12345";
+		String tripType = "I";
+		
+		clientPricing.setCmpid(1);
+		clientPricing.setTripType("D");
+		clientPricings.add(clientPricing);
+		client.setClientPricings(clientPricings);
+		
+		when(repository.getClient(clientAccountNumber)).thenReturn(client);
+		List<ClientPricing> result = service.getClientPricings(clientAccountNumber, tripType);
+		
+		assertTrue(result.isEmpty());
 		verify(repository, times(1)).getClient(clientAccountNumber);
 	}
 	
