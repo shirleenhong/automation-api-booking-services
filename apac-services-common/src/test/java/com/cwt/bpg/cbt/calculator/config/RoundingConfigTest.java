8e9d60c55e4d4796864eb7b9a1468601b848b0a4
Jay-cwt <Ahmad.Ali@carlsonwagonlit.com>
2018-10-30 15:16:23 +0800

    Fixed unit tests of RoundingConfig

--- apac-services-common/src/test/java/com/cwt/bpg/cbt/calculator/config/RoundingConfigTest.java
+++ apac-services-common/src/test/java/com/cwt/bpg/cbt/calculator/config/RoundingConfigTest.java
@@ -25,35 +25,35 @@ public class RoundingConfigTest {
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		
-		Mockito.when(env.getProperty(Mockito.eq("com.cwt.bpg.cbt.calc.rounding.nettFare.Default"), Mockito.eq(Integer.class))).thenReturn(0);
-		Mockito.when(env.getProperty(Mockito.eq("com.cwt.bpg.cbt.calc.rounding.nettCost.Default"), Mockito.eq(Integer.class))).thenReturn(0);
-		Mockito.when(env.getProperty(Mockito.eq("com.cwt.bpg.cbt.calc.rounding.merchantFee.Default"), Mockito.eq(Integer.class))).thenReturn(0);
-		Mockito.when(env.getProperty(Mockito.eq("com.cwt.bpg.cbt.calc.rounding.totalSellingFare.Default"), Mockito.eq(Integer.class))).thenReturn(0);
-		Mockito.when(env.getProperty(Mockito.eq("com.cwt.bpg.cbt.calc.rounding.commission.Default"), Mockito.eq(Integer.class))).thenReturn(1);
+		Mockito.when(env.getRequiredProperty("com.cwt.bpg.cbt.calc.rounding.nettFare.Default", Integer.class)).thenReturn(0);
+		Mockito.when(env.getRequiredProperty("com.cwt.bpg.cbt.calc.rounding.nettCost.Default", Integer.class)).thenReturn(0);
+		Mockito.when(env.getRequiredProperty("com.cwt.bpg.cbt.calc.rounding.merchantFee.Default", Integer.class)).thenReturn(0);
+		Mockito.when(env.getRequiredProperty("com.cwt.bpg.cbt.calc.rounding.totalSellingFare.Default", Integer.class)).thenReturn(0);
+		Mockito.when(env.getRequiredProperty("com.cwt.bpg.cbt.calc.rounding.commission.Default", Integer.class)).thenReturn(1);
 	}
 	
-//	@Test
-//	public void shouldReturnNettFareRoundingWhenCountryCodeIsNull() {
-//		assertEquals(RoundingMode.UP, config.getRoundingMode("nettFare", null));
-//	}
-//
-//	@Test
-//	public void shouldReturnNettCostRounding() {
-//		assertEquals(RoundingMode.UP, config.getRoundingMode("nettCost", "SG"));
-//	}
-//
-//	@Test
-//	public void shouldReturnMerchantFeeRounding() {
-//		assertEquals(RoundingMode.UP, config.getRoundingMode("merchantFee", "HK"));
-//	}
-//
-//	@Test
-//	public void shouldReturnTotalSellingFareRounding() {
-//		assertEquals(RoundingMode.UP, config.getRoundingMode("totalSellingFare", "HK"));
-//	}
-//
-//	@Test
-//	public void shouldReturnCommissionRounding() {
-//		assertEquals(RoundingMode.DOWN, config.getRoundingMode("commission", "HK"));
-//	}
+	@Test
+	public void shouldReturnNettFareRoundingWhenCountryCodeIsNull() {
+		assertEquals(RoundingMode.UP, config.getRoundingMode("nettFare", null));
+	}
+
+	@Test
+	public void shouldReturnNettCostRounding() {
+		assertEquals(RoundingMode.UP, config.getRoundingMode("nettCost", "SG"));
+	}
+
+	@Test
+	public void shouldReturnMerchantFeeRounding() {
+		assertEquals(RoundingMode.UP, config.getRoundingMode("merchantFee", "HK"));
+	}
+
+	@Test
+	public void shouldReturnTotalSellingFareRounding() {
+		assertEquals(RoundingMode.UP, config.getRoundingMode("totalSellingFare", "HK"));
+	}
+
+	@Test
+	public void shouldReturnCommissionRounding() {
+		assertEquals(RoundingMode.DOWN, config.getRoundingMode("commission", "HK"));
+	}
 }
