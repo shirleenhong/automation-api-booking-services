4ca6e1ce4cc2485667df60fd68aa4c4e77aaeec8
mosesbrian.calma <mosesbrian.calma@carlsonwagonlit.com>
2018-11-13 18:41:42 +0800

    removed creditCard and on FormOfPayment , also removed CC validators

--- apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/validator/CcNotNullOnFopType.java
+++ apac-services-exchange-order-module/apac-services-exchange-order-api/src/main/java/com/cwt/bpg/cbt/exchange/order/model/validator/CcNotNullOnFopType.java
@@ -1,14 +1,14 @@
 package com.cwt.bpg.cbt.exchange.order.model.validator;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import javax.validation.Payload;
 
 /**
  * Validates that field {@code dependentFields} is not null if
@@ -17,7 +17,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
-@Constraint(validatedBy = CcNotNullOnInsertOnFopTypeValidator.class)
 @Documented
 public @interface CcNotNullOnFopType {
     String[] fopTypes();
