d71e34aa647e6f23016cea3da77e9cc04e5d0222
alpatricio <annajosedel.patricio@carlsonwagonlit.com>
2019-06-14 16:10:43 +0800

    Cipher not secure

--- apac-services-encryptor/src/main/java/com/cwt/bpg/cbt/encryptor/impl/Encryptor.java
+++ apac-services-encryptor/src/main/java/com/cwt/bpg/cbt/encryptor/impl/Encryptor.java
@@ -1,72 +1,65 @@
 package com.cwt.bpg.cbt.encryptor.impl;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Base64;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class Encryptor
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(Encryptor.class);
 
-    private SecretKeySpec secretKeySpec;
-
-    private String secretKey;
+    private SecretKey secretKey;
 
     public Encryptor(String secretKey)
     {
-        this.secretKey = secretKey;
-        try
-        {
-            byte[] key = secretKey.getBytes("UTF-8");
-            key = MessageDigest.getInstance("SHA-1").digest(key);
-            key = Arrays.copyOf(key, 16);
-            this.secretKeySpec = new SecretKeySpec(key, "AES");
-        }
-        catch (NoSuchAlgorithmException | UnsupportedEncodingException e)
-        {
-            LOGGER.error(e.getMessage(), e);
-        }
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(), "AES");
     }
 
-    public String encrypt(String strToEncrypt)
-    {
-        try
-        {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-        }
-        catch (Exception e)
-        {
-            LOGGER.error(e.getMessage(), e);
-        }
-        return null;
+    private final static int GCM_IV_LENGTH = 12;
+    private final static int GCM_TAG_LENGTH = 16;
+
+    public String encrypt(String privateString) throws Exception {
+        byte[] iv = new byte[GCM_IV_LENGTH];
+        (new SecureRandom()).nextBytes(iv);
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec ivSpec = new GCMParameterSpec(GCM_TAG_LENGTH * Byte.SIZE, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
+
+        byte[] ciphertext = cipher.doFinal(privateString.getBytes("UTF8"));
+        byte[] encrypted = new byte[iv.length + ciphertext.length];
+        System.arraycopy(iv, 0, encrypted, 0, iv.length);
+        System.arraycopy(ciphertext, 0, encrypted, iv.length, ciphertext.length);
+
+        String encoded = Base64.getEncoder().encodeToString(encrypted);
+
+        return encoded;
     }
 
-    public String decrypt(String strToDecrypt)
-    {
-        try
-        {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        }
-        catch (Exception e)
-        {
-            LOGGER.error(e.getMessage(), e);
-        }
-        return null;
+    public String decrypt(String encrypted) throws Exception {
+        byte[] decoded = Base64.getDecoder().decode(encrypted);
+
+        byte[] iv = Arrays.copyOfRange(decoded, 0, GCM_IV_LENGTH);
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec ivSpec = new GCMParameterSpec(GCM_TAG_LENGTH * Byte.SIZE, iv);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+
+        byte[] ciphertext = cipher.doFinal(decoded, GCM_IV_LENGTH, decoded.length - GCM_IV_LENGTH);
+
+        String result = new String(ciphertext, "UTF8");
+
+        return result;
     }
 
-    public String getSecretKey()
+    public SecretKey getSecretKey()
     {
         return secretKey;
     }
