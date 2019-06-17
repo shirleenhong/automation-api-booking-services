0b8669615c00726d15032fbc07663a8fd2b75cab
juliusmercons <julius.masanting@carlsonwagonlit.com>
2019-06-17 14:02:47 +0800

    removed erroneous decrypted text from EncryptorTest

--- apac-services-encryptor/src/test/java/com/cwt/bpg/cbt/encryptor/impl/EncryptorTest.java
+++ apac-services-encryptor/src/test/java/com/cwt/bpg/cbt/encryptor/impl/EncryptorTest.java
@@ -17,6 +17,5 @@ public class EncryptorTest {
         System.out.println(encryptor.decrypt("1pg+B9VgliM0N058AgIQFPOQvtIkgT9Ung31jK6PHMvTTkQWIpy3yg3JCmpY"));
         System.out.println(encryptor.decrypt("IZ8bjG6vgDuU86Ar9kTkxUOaGfqVTjDFINI501f2fsdE"));
         System.out.println(encryptor.decrypt("3gd5Y8GzKMA5ORKX2FnKQ4AXVHxNiFynShbiT+w80VEnnj+etJGojw8="));
-        System.out.println(encryptor.decrypt("Ve+/TcCQfdXjvueCLmIiy6FDr0M2tcyk7JQ+49aGVcs="));
     }
 }
