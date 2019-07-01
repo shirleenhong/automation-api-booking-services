bec6dc75977f4fa2a9a0996150f230445566e3b6
Jerwin Uy <JerwinLouise.Uy@carlsonwagonlit.com>
2019-07-01 18:12:06 +0800

    added configuration for sandbox; change loading of properties so that it will be parametrized

--- apac-services-common/src/main/java/com/cwt/bpg/cbt/calculator/config/CommonPropertyConfig.java
+++ apac-services-common/src/main/java/com/cwt/bpg/cbt/calculator/config/CommonPropertyConfig.java
@@ -1,52 +1,17 @@
 package com.cwt.bpg.cbt.calculator.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration("com.cwt.bpg.cbt.calculator.property.config")
-public class CommonPropertyConfig {
+public class CommonPropertyConfig
+{
 
-	@Configuration("com.cwt.bpg.cbt.calculator.config.source")
-	@Profile("local")
-	@PropertySource(value = { "classpath:properties/common-local.properties" }, ignoreResourceNotFound = true)
-	static class LocalPropertyConfig {
-	}
+    @Configuration("com.cwt.bpg.cbt.calculator.config.source")
+    @PropertySource(value = { "classpath:properties/common-${spring.profiles.active}.properties" }, ignoreResourceNotFound = true)
+    static class PropertyConfig
+    {
 
-	@Configuration("com.cwt.bpg.cbt.calculator.config.source")
-	@Profile("dev")
-	@PropertySource(value = { "classpath:properties/common-dev.properties" }, ignoreResourceNotFound = true)
-	static class DevPropertyConfig {
-	}
-
-	@Configuration("com.cwt.bpg.cbt.calculator.config.source")
-	@Profile("int")
-	@PropertySource(value = { "classpath:properties/common-int.properties" }, ignoreResourceNotFound = true)
-	static class IntPropertyConfig {
-	}
-
-	@Configuration("com.cwt.bpg.cbt.calculator.config.source")
-	@Profile("preprod")
-	@PropertySource(value = { "classpath:properties/common-preprod.properties" }, ignoreResourceNotFound = true)
-	static class PreprodPropertyConfig {
-	}
-
-	@Configuration("com.cwt.bpg.cbt.calculator.config.source")
-	@Profile("prod")
-	@PropertySource(value = { "classpath:properties/common-prod.properties" }, ignoreResourceNotFound = true)
-	static class ProdPropertyConfig {
-	}
-
-	@Configuration("com.cwt.bpg.cbt.calculator.config.source")
-	@Profile("perf")
-	@PropertySource(value = { "classpath:properties/common-perf.properties" }, ignoreResourceNotFound = true)
-	static class PerfPropertyConfig {
-	}
-
-	@Configuration("com.cwt.bpg.cbt.calculator.config.source")
-	@Profile("sec")
-	@PropertySource(value = { "classpath:properties/common-sec.properties" }, ignoreResourceNotFound = true)
-	static class SecurityPropertyConfig {
-	}
+    }
 
 }
--- apac-services-encryptor/src/main/java/com/cwt/bpg/cbt/encryptor/impl/config/PropertyConfig.java
+++ apac-services-encryptor/src/main/java/com/cwt/bpg/cbt/encryptor/impl/config/PropertyConfig.java
@@ -1,59 +1,16 @@
 package com.cwt.bpg.cbt.encryptor.impl.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration("com.cwt.encryptor.impl.property.config")
 public class PropertyConfig
 {
     @Configuration("com.cwt.encryptor.impl.property.config.source")
-    @Profile("local")
-    @PropertySource(value = { "classpath:properties/encryptor-local.properties" }, ignoreResourceNotFound = true)
-    static class LocalPropertyConfig
+    @PropertySource(value = { "classpath:properties/encryptor-${spring.profiles.active}.properties" }, ignoreResourceNotFound = true)
+    static class PropertyConfigSource
     {
-    }
-    
-    @Configuration("com.cwt.encryptor.impl.property.config.source")
-    @Profile("dev")
-    @PropertySource(value = { "classpath:properties/encryptor-dev.properties" }, ignoreResourceNotFound = true)
-    static class DevPropertyConfig
-    {
-    }
 
-    @Configuration("com.cwt.encryptor.impl.property.config.source")
-    @Profile("int")
-    @PropertySource(value = { "classpath:properties/encryptor-int.properties" }, ignoreResourceNotFound = true)
-    static class IntPropertyConfig
-    {
-    }
-
-    @Configuration("com.cwt.encryptor.impl.property.config.source")
-    @Profile("preprod")
-    @PropertySource(value = { "classpath:properties/encryptor-preprod.properties" }, ignoreResourceNotFound = true)
-    static class PreprodPropertyConfig
-    {
-    }
-
-    @Configuration("com.cwt.encryptor.impl.property.config.source")
-    @Profile("prod")
-    @PropertySource(value = { "classpath:properties/encryptor-prod.properties" }, ignoreResourceNotFound = true)
-    static class ProdPropertyConfig
-    {
-    }
-
-    @Configuration("com.cwt.encryptor.impl.property.config.source")
-    @Profile("perf")
-    @PropertySource(value = { "classpath:properties/encryptor-perf.properties" }, ignoreResourceNotFound = true)
-    static class PerfPropertyConfig
-    {
-    }
-
-    @Configuration("com.cwt.encryptor.impl.property.config.source")
-    @Profile("sec")
-    @PropertySource(value = { "classpath:properties/encryptor-sec.properties" }, ignoreResourceNotFound = true)
-    static class SecurityPropertyConfig
-    {
     }
 
 }
