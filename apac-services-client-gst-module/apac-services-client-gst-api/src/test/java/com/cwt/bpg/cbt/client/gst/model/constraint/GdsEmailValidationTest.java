285b25a9dd6340523d676822b28f65df03dbcfe6
puigterence <puigterence@carlsonwagonlit.com>
2019-07-16 14:59:08 +0800

    formatting

--- apac-services-client-gst-module/apac-services-client-gst-api/src/test/java/com/cwt/bpg/cbt/client/gst/model/constraint/GdsEmailValidationTest.java
+++ apac-services-client-gst-module/apac-services-client-gst-api/src/test/java/com/cwt/bpg/cbt/client/gst/model/constraint/GdsEmailValidationTest.java
@@ -12,28 +12,27 @@ import com.cwt.bpg.cbt.client.gst.model.ClientGstInfo;
 
 public class GdsEmailValidationTest {
 
-	@Test
-	public void canValidateGdsEmailFormat() {
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
+    @Test
+    public void canValidateGdsEmailFormat() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
 
-		ClientGstInfo clientGstInfo = new ClientGstInfo();
-		clientGstInfo.setGstin("1234567890ABCDE");
-		clientGstInfo.setClient("client");
-		clientGstInfo.setClientEntityName("clientEntityName");
-		clientGstInfo.setBusinessPhoneNumber("2211996");
-		clientGstInfo.setEntityAddressLine1("entityAddressLine1");
-		clientGstInfo.setPostalCode("9200");
-		clientGstInfo.setCity("city");
-		clientGstInfo.setState("state");
-		clientGstInfo.setBusinessEmailAddress("sample-mini_test1234567@sgmail.com");
+        ClientGstInfo clientGstInfo = new ClientGstInfo();
+        clientGstInfo.setGstin("1234567890ABCDE");
+        clientGstInfo.setClient("client");
+        clientGstInfo.setClientEntityName("clientEntityName");
+        clientGstInfo.setBusinessPhoneNumber("2211996");
+        clientGstInfo.setEntityAddressLine1("entityAddressLine1");
+        clientGstInfo.setPostalCode("9200");
+        clientGstInfo.setCity("city");
+        clientGstInfo.setState("state");
+        clientGstInfo.setBusinessEmailAddress("sample-mini_test1234567@sgmail.com");
 
-		Set<ConstraintViolation<ClientGstInfo>> constraintViolations = validator
-				.validate(clientGstInfo);
+        Set<ConstraintViolation<ClientGstInfo>> constraintViolations = validator.validate(clientGstInfo);
 
-		assertEquals(1, constraintViolations.size());
-		assertEquals("GDS Email is not valid or more than 35 chars after transformation",
-				constraintViolations.iterator().next().getMessage());
-	}
+        assertEquals(1, constraintViolations.size());
+        assertEquals("GDS Email is not valid or more than 35 chars after transformation",
+                constraintViolations.iterator().next().getMessage());
+    }
 
 }
