eff59f24da292ce01ebe173dec31ff8408de163f
argel <argel.castillo@carlsonwagonlit.com>
2019-07-31 15:48:44 +0800

    air transaction migration

--- tpro-database-migration/src/main/java/com/cwt/bpg/cbt/tpromigration/csv/converter/AirTransactionConverter.java
+++ tpro-database-migration/src/main/java/com/cwt/bpg/cbt/tpromigration/csv/converter/AirTransactionConverter.java
@@ -1,10 +1,14 @@
 package com.cwt.bpg.cbt.tpromigration.csv.converter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.cwt.bpg.cbt.air.transaction.model.AirTransaction;
-import com.cwt.bpg.cbt.exchange.order.model.india.AirMiscInfo;
+import com.cwt.bpg.cbt.air.transaction.model.PassthroughType;
 
 public class AirTransactionConverter implements Converter<Map<String, String>, AirTransaction>
 {
@@ -12,8 +16,37 @@ public class AirTransactionConverter implements Converter<Map<String, String>, A
     @Override
     public void convert(Map<String, String> source, List<AirTransaction> results)
     {
-        // TODO Auto-generated method stub
-        
+        final AirTransaction airTransaction = new AirTransaction();
+
+        airTransaction.setAirlineCode(StringUtils.trim(source.get("airlineCode")));
+        airTransaction.setAirlineDescription(StringUtils.trim(source.get("airlineDescription")));
+        airTransaction.setCcVendorCode(StringUtils.trim(source.get("ccVendorCode")));
+        airTransaction.setCcVendorName(StringUtils.trim(source.get("ccVendorName")));
+        airTransaction.setCcType(StringUtils.trim(source.get("ccType")));
+        airTransaction.setPassthroughType(PassthroughType.fromString(StringUtils.trim(source.get("passthroughType"))));
+        airTransaction.setCountryCode(StringUtils.trim(source.get("countryCode")));
+        airTransaction.setBookingClasses(getBookingClasses(StringUtils.trim(source.get("bookingClasses"))));
+
+        results.add(airTransaction);
     }
 
+    private List<String> getBookingClasses(String bookingClass)
+    {
+        if (StringUtils.isNotEmpty(bookingClass))
+        {
+
+            final String[] bookingClassValue = bookingClass.split(",");
+            List<String> bookingClasses = new ArrayList<>();
+
+            for (final String val : bookingClassValue)
+            {
+                bookingClasses.add(val);
+            }
+
+            return bookingClasses;
+
+        }
+
+        return Collections.emptyList();
+    }
 }
